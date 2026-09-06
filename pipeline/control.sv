b7817341c710d8cbd489c84781f286734cca1cf3
juheon727 <juheon727@gmail.com>
2026-09-06 16:21:47 +0900

    Cleaned files and control logic for pipelined processor

--- pipeline/control.sv
+++ pipeline/control.sv
@@ -12,6 +12,47 @@ assign imm_out = {
 
 endmodule
 
+task automatic ld_and_sd (
+    input logic [6:0] opcode,
+    input logic [2:0] funct3,
+    input logic [31:0] instr,
+    inout logic reg_write,
+    inout logic use_imm_in_alu,
+    inout logic [11:0] imm12,
+    inout logic [3:0] alu_opcode,
+    inout logic dmem_read,
+    inout logic dmem_write,
+    inout logic load_instr,
+    inout logic illegal
+);
+
+case (opcode)
+    //ld
+    7'b0000011: begin
+        if (funct3 == 3'b011) begin
+            reg_write = 1'b1;
+            use_imm_in_alu = 1'b1;
+            imm12 = instr[31:20];
+            alu_opcode = 4'b0010;
+            dmem_read = 1'b1;
+            load_instr = 1'b1;
+            illegal = 1'b0;
+        end
+    end
+    //sd
+    7'b0100011: begin
+        if (funct3 == 3'b011) begin
+            use_imm_in_alu = 1'b1;
+            imm12 = {instr[31:25], instr[11:7]};
+            alu_opcode = 4'b0010;
+            dmem_write = 1'b1;
+            illegal = 1'b0;
+        end
+    end
+endcase
+
+endtask
+
 task automatic rtype_decode (
     input logic [6:0] funct7,
     input logic [2:0] funct3,
@@ -45,6 +86,42 @@ endcase
 
 endtask
 
+task automatic immediate_arithmetic_decode (
+    input logic [2:0] funct3,
+    input logic [31:0] instr,
+    output logic reg_write,
+    output logic [3:0] alu_opcode,
+    output logic use_imm_in_alu,
+    output logic [11:0] imm12,
+    output logic illegal
+);
+
+case (funct3)
+    3'b111: begin
+        reg_write = 1'b1;
+        alu_opcode = 4'b0;
+        use_imm_in_alu = 1'b1;
+        imm12 = instr[31:20];
+        illegal = 1'b0;
+    end
+    3'b110: begin
+        reg_write = 1'b1;
+        alu_opcode = 4'b0001;
+        use_imm_in_alu = 1'b1;
+        imm12 = instr[31:20];
+        illegal = 1'b0;
+    end
+    3'b000: begin
+        reg_write = 1'b1;
+        alu_opcode = 4'b0010;
+        use_imm_in_alu = 1'b1;
+        imm12 = instr[31:20];
+        illegal = 1'b0;
+    end
+endcase
+
+endtask
+
 module riscv_subset_decoder (
     input logic [31:0] instr,
     output logic reg_write,
@@ -70,24 +147,13 @@ assign funct3 = instr[14:12];
 assign funct7 = instr[31:25];
 
 logic [11:0] imm12;
-logic [12:0] imm13;
-logic [63:0] imm_mem;
-logic [63:0] imm_b;
 
 imm_sign_extend #(
     .IMM_WIDTH_IN(12),
     .IMM_WIDTH_OUT(64)
-) imm_extender_mem (
+) imm_extender (
     .imm_in(imm12),
-    .imm_out(imm_mem)
-);
-
-imm_sign_extend #(
-    .IMM_WIDTH_IN(13),
-    .IMM_WIDTH_OUT(64)
-) imm_extender_b (
-    .imm_in(imm13),
-    .imm_out(imm_b)
+    .imm_out(imm)
 );
 
 assign reg_waddr = instr[11:7];
@@ -98,8 +164,6 @@ always_comb begin
     reg_write = 1'b0;
     use_imm_in_alu = 1'b0;
     imm12 = 12'b0;
-    imm13 = 13'b0;
-    imm = 64'b0;
     alu_opcode = 4'b0;
     dmem_read = 1'b0;
     dmem_write = 1'b0;
@@ -107,44 +171,32 @@ always_comb begin
     branch = 1'b0;
     illegal = 1'b1;
 
+    ld_and_sd(
+        opcode,
+        funct3,
+        instr,
+        reg_write,
+        use_imm_in_alu,
+        imm12,
+        alu_opcode,
+        dmem_read,
+        dmem_write,
+        load_instr,
+        illegal
+    );
+
     case (opcode)
-        //ld
-        7'b0000011: begin
-            if (funct3 == 3'b011) begin
-                reg_write = 1'b1;
-                use_imm_in_alu = 1'b1;
-                imm12 = instr[31:20];
-                imm = imm_mem;
-                alu_opcode = 4'b0010;
-                dmem_read = 1'b1;
-                load_instr = 1'b1;
-                illegal = 1'b0;
-            end
-        end
-        //sd
-        7'b0100011: begin
-            if (funct3 == 3'b011) begin
-                use_imm_in_alu = 1'b1;
-                imm12 = {instr[31:25], instr[11:7]};
-                imm = imm_mem;
-                alu_opcode = 4'b0010;
-                dmem_write = 1'b1;
-                illegal = 1'b0;
-            end
-        end
-        //beq
+        //beq: SL1 will be outsourced to PC update logic.
         7'b1100011: begin
             if (funct3 == 3'b000) begin
                 alu_opcode = 4'b0110;
                 branch = 1'b1;
-                imm13 = {
+                imm12 = {
                     instr[31],
                     instr[7],
                     instr[30:25],
-                    instr[11:8],
-                    1'b0
+                    instr[11:8]
                 };
-                imm = imm_b;
                 illegal = 1'b0;
             end
         end
@@ -154,32 +206,7 @@ always_comb begin
         end
         //immediate arithmetic: andi, ori, addi
         7'b0010011: begin
-            case (funct3)
-                3'b111: begin
-                    reg_write = 1'b1;
-                    alu_opcode = 4'b0;
-                    use_imm_in_alu = 1'b1;
-                    imm12 = instr[31:20];
-                    imm = imm_mem;
-                    illegal = 1'b0;
-                end
-                3'b110: begin
-                    reg_write = 1'b1;
-                    alu_opcode = 4'b0001;
-                    use_imm_in_alu = 1'b1;
-                    imm12 = instr[31:20];
-                    imm = imm_mem;
-                    illegal = 1'b0;
-                end
-                3'b000: begin
-                    reg_write = 1'b1;
-                    alu_opcode = 4'b0010;
-                    use_imm_in_alu = 1'b1;
-                    imm12 = instr[31:20];
-                    imm = imm_mem;
-                    illegal = 1'b0;
-                end
-            endcase
+            immediate_arithmetic_decode(funct3, instr, reg_write, alu_opcode, use_imm_in_alu, imm12, illegal);
         end
     endcase
 end
